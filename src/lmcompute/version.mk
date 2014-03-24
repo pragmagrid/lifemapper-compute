3358e257d1d5f7bf9abca51ddd5c0e8a8908ee44
nadyawilliams <nadya@sdsc.edu>
2014-03-24 16:31:06 -0700

    updates for refactored lifemapper distro

--- src/lmcompute/version.mk
+++ src/lmcompute/version.mk
@@ -1,14 +1,17 @@
-PKGROOT		= /opt/lifemapper
+LMBASE		= /opt/lifemapper
+PKGROOT		= /opt/lifemapper/LmCompute
 LMDISK          = /share/lm
 NAME        	= lifemapper-lmcompute
-PKGNAME         = lmCompute
-INSTDIR		= lm
+PKGNAME         = LmCompute
+#INSTDIR		= LmCompute
 VERSION     	= 1.0
 RELEASE 	= 0
 TARBALL_POSTFIX	= tar.gz
 
-PYBIN          = /opt/python/bin/python
-LMUSER           = pragma,lm2
+PYBIN           = /opt/python/bin/python2.7
+LMUSER          = pragma,lm2
+
+DIST            = 20140314
 
 RPM.EXTRAS = %define __os_install_post %{nil}
 
