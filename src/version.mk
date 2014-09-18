bf3a6f4bfc0f5d136e464b0b98c3f69b22437196
nadyawilliams <nadya@sdsc.edu>
2014-09-18 14:30:15 -0700

    combine  configs for server and lmcompute need to keep in sync src/version.mk files for both lifemapper rolls

--- src/version.mk
+++ src/version.mk
@@ -1,5 +1,14 @@
-PKGROOT		= /opt/lifemapper
-LMHOME		= /opt/lifemapper
-LMDISK          = /share/lm
-LMUSER          = pragma,lm2
-PYTHONVER       = python2.7
+PKGROOT       = /opt/lifemapper
+LMHOME        = /opt/lifemapper
+MAPSERVER_TMP = /var/www/tmp
+LMWEB         = /var/lib/lm2
+DATADIR       = /share/lmserver/data
+TEMPDIR       = /tmp
+PYTHONVER     = python2.7
+PGSQLVER      = 9.1
+LMDISK        = /share/lm
+LMUSER        = pragma,lm2
+UNIXSOCKET    = /var/run/postgresql
+SMTPSERVER    = localhost
+SMTPSENDER    = no-reply-lifemapper@@PUBLIC_FQDN@
+
