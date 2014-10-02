0e4d6712774e6fb439b6a51a9170382af62c8165
root <root@fe.public>
2014-10-01 17:27:50 -0700

    add downloaded RPMS

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,18 +6,22 @@
 yum --enablerepo base install cmake.x86_64;
 yum --enablerepo base install subversion.x86_64;
 
-(cd src/RPMS; 
-yumdownloader --resolve --enablerepo base screen.x86_64; \
-yumdownloader --resolve --enablerepo base gsl.x86_64; \
-yumdownloader --resolve --enablerepo base gsl-devel.x86_64; \
-yumdownloader --resolve --enablerepo base sqlite-devel.x86_64; \
-yumdownloader --resolve --enablerepo rpmforge hdf4.x86_64; \
-yumdownloader --resolve --enablerepo rpmforge hdf4-devel.x86_64; \
-yumdownloader --resolve --enablerepo rpmforge hdf5.x86_64; \
-yumdownloader --resolve --enablerepo rpmforge hdf5-devel.x86_64; \
-)
+# Do once for roll repo
+#(cd src/RPMS; 
+#yumdownloader --resolve --enablerepo base screen.x86_64; \
+#yumdownloader --resolve --enablerepo base gsl.x86_64; \
+#yumdownloader --resolve --enablerepo base gsl-devel.x86_64; \
+#yumdownloader --resolve --enablerepo base sqlite-devel.x86_64; \
+#yumdownloader --resolve --enablerepo rpmforge hdf4.x86_64; \
+#yumdownloader --resolve --enablerepo rpmforge hdf4-devel.x86_64; \
+#yumdownloader --resolve --enablerepo rpmforge hdf5.x86_64; \
+#yumdownloader --resolve --enablerepo rpmforge hdf5-devel.x86_64; \
+#)
 
 echo "/opt/lifemapper/lib" > /etc/ld.so.conf.d/lifemapper.conf
+
+# for gdal
+rpm -i src/RPMS/hdf5*rpm
 
 # install proj
 compile proj
@@ -51,9 +55,6 @@ install lifemapper-spatialindex
 # for pysal
 compile scipy
 install opt-lifemapper-scipy
-
-# for gdal
-yum --enablerepo rpmforge install hdf5 hdf5-devel
 
 echo "You will need to checkout src from Lifemapper SVN:"
 echo "    cd src/lmcompute"
