8cb6f044ff9b7fb6659c868d94b1114713310cdc
Aimee Stewart <zzeppozz@gmail.com>
2015-05-13 17:18:26 -0500

    corrected DATADIR

--- src/version.mk
+++ src/version.mk
@@ -2,13 +2,15 @@ PKGROOT       = /opt/lifemapper
 LMHOME        = /opt/lifemapper
 MAPSERVER_TMP = /var/www/tmp
 LMWEB         = /var/lib/lm2
-DATADIR       = /share/lmserver/data
+DATADIR       = /share/lm/data
 TEMPDIR       = /tmp
 PYTHONVER     = python2.7
 PGSQLVER      = 9.1
 LMDISK        = /share/lm
-LMUSER        = pragma
 UNIXSOCKET    = /var/run/postgresql
 SMTPSERVER    = localhost
 SMTPSENDER    = no-reply-lifemapper@@PUBLIC_FQDN@
 JAVABIN       = /usr/java/latest/bin/java
+
+LMUSER                = idigbio
+SCENARIO_PACKAGE      = WC_AR5_5min
