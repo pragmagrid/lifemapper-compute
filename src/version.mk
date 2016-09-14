12d9ba70e0fec30398768aee2261c25355d92453
Aimee Stewart <zzeppozz@gmail.com>
2016-09-14 17:38:46 -0500

    comments for next step

--- src/version.mk
+++ src/version.mk
@@ -4,7 +4,15 @@ MAPSERVER_TMP = /var/www/tmp
 LMWEB         = /var/lib/lm2
 LMURL			  = http://lifemapper.org/dl
 DATADIR_COMPUTE   = /share/lm/data
+
+# TODO:
+# DATADIR_COMPUTE   = /share/lmcompute/data
+# This variable will be identical in the lifemapper-server roll
+# DATADIR_SHARED  = /share/lm/data
+
+# This variable is identical to ENV_DATA_DIR in the lifemapper-server roll
 INPUT_LAYER_DIR   = layers
+
 INPUT_LAYER_DB    = layers.db
 TEMPDIR       = /tmp
 PYTHONVER     = python2.7
