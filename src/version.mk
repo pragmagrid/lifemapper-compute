e03ecdf2fd5819e9be676ba731bb7f892bd93d5a
Aimee Stewart <zzeppozz@gmail.com>
2016-10-12 17:26:52 -0500

    rearrange and comment

--- src/version.mk
+++ src/version.mk
@@ -3,25 +3,25 @@ LMHOME        = /opt/lifemapper
 MAPSERVER_TMP = /var/www/tmp
 LMWEB         = /var/lib/lm2
 LMURL			  = http://lifemapper.org/dl
-DATADIR_COMPUTE   = /share/lm/data
-
+LMDISK        = /share/lm
 # This variable is identical in the lifemapper-server roll
 DATADIR_SHARED  = /share/lm/data
+DATADIR_COMPUTE   = /share/lm/data
+LMSCRATCHDISK    = /state/partition1/lmscratch
+TEMPDIR       = /tmp
 
-# This variable is identical to ENV_DATA_DIR in the lifemapper-server roll
+# This variable matches ENV_DATA_DIR in the lifemapper-server roll
 INPUT_LAYER_DIR   = layers
 
 INPUT_LAYER_DB    = layers.db
-TEMPDIR       = /tmp
 PYTHONVER     = python2.7
 PGSQLVER      = 9.1
-LMDISK        = /share/lm
-LMSCRATCHDISK    = /state/partition1/lmscratch
 UNIXSOCKET    = /var/run/postgresql
 SMTPSERVER    = localhost
 SMTPSENDER    = no-reply-lifemapper@@PUBLIC_FQDN@
 JAVABIN       = /usr/java/latest/bin/java
 
+# This variable matches SCENARIO_PACKAGE in the lifemapper-server roll
 SCENARIO_PACKAGE_SEED    = 10min-past-present-future
 
 # Options are local or cluster
