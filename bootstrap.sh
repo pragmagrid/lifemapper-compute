cbee5acaa2957714f9bc397bb8e08743f4ec38bc
zzeppozz <aimee.stewart@ku.edu>
2018-04-09 08:18:00 -0500

    RPM files; enable/disable opt-python

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 . /opt/rocks/share/devel/src/roll/etc/bootstrap-functions.sh
 
+module unload opt-python
 # download needed RPMS
 yum --enablerepo base install cmake.x86_64;
 
@@ -27,22 +28,30 @@ rpm -i src/RPMS/sqlite-devel*rpm
 rpm -i src/RPMS/screen*rpm
 
 # install proj
+module load opt-python
 compile proj
+module unload opt-python
 install lifemapper-proj
 /sbin/ldconfig
 
 # install tiff
+module load opt-python
 compile tiff
+module unload opt-python
 install lifemapper-tiff
 /sbin/ldconfig
 
 # need for gdal
+module load opt-python
 compile geos
+module unload opt-python
 install lifemapper-geos
 /sbin/ldconfig
 
 # need for modules
+module load opt-python
 compile gdal
+module unload opt-python
 install lifemapper-gdal
 /sbin/ldconfig
 
@@ -51,23 +60,22 @@ install lifemapper-gdal
 #install opt-lifemapper-setuptools
 
 # for rtree
+module load opt-python
 compile spatialindex
+module unload opt-python
 install lifemapper-spatialindex
 /sbin/ldconfig
 
 # for pysal
+module load opt-python
 compile scipy
+module unload opt-python
 install opt-lifemapper-scipy
 
-echo "You will need to checkout Lifemapper src from Github:"
-echo "    cd src/lmcompute"
-echo "    make prep "
-echo "and download data from Lifemapper:"
-echo "    cd src/lmdata-env"
-echo "    make prep "
-echo "finally download CCTools source code:"
-echo "    cd src/cctools"
-echo "    make prep "
-echo "and DendroPy source code:"
-echo "    cd src/dendropy"
+echo "You will need to download source code, data and dependencies."
+echo "    lmcompute"
+echo "    lmdata-env"
+echo "    cctools"
+echo "    dendropy"
+echo "Go to each of the packages and execute:"
 echo "    make prep "
