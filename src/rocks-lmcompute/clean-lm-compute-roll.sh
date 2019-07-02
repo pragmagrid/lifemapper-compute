0fd693b3388ce16d8bc73eeb8cd7c82c3ee7a0bb
Aimee Stewart <aimee.stewart@ku.edu>
2019-07-02 11:17:59 -0500

    cleanup clean

--- src/rocks-lmcompute/clean-lm-compute-roll.sh
+++ src/rocks-lmcompute/clean-lm-compute-roll.sh
@@ -21,56 +21,50 @@ set_defaults() {
 }
 
 
-del-shared-geospatial-dependencies() {
-   echo "Removing shared geos, proj, tiff, and gdal dependencies RPMS" >> $LOG
-   $RM CharLS
-   $RM SuperLU
-   $RM armadillo
-   $RM arpack
-   $RM blas
-   $RM atlas
-   $RM cfitsio
-   $RM freexl
-   $RM gpsbabel
-   $RM lapack
-   $RM geos geos-devel  geos-python
-   $RM hdf5-devel
-   $RM libaec
-   $RM glibc     
-   $RM jbigkit-libs     
-   $RM libgcc     
-   $RM libjpeg-turbo     
-   $RM libstdc++
-   $RM libtiff  libtiff-devel  
-   $RM nss-softokn-freebl     
-   $RM zlib
-   $RM libgeotiff libgeotiff-devel
-   $RM libdap
-   $RM libusb
-   $RM libgta
-   $RM ogdi
-   $RM netcdf
-   $RM openblas-openmp     
-   $RM postgresql-libs
-   $RM openjpeg2
-   $RM unixODBC
-   $RM xerces-c
-   $RM proj49 proj49-devel proj49-epsg proj49-nad
-   $RM shapelib
-   $RM gdal gdal-libs gdal-devel gdal-python 
-   $RM python-nose     
-   $RM numpy     
-   $RM gdal-python
-}
-
 del-lifemapper-shared() {
-   echo "Removing SHARED lifemapper-* and prerequisite RPMS" >> $LOG
+   echo "Removing shared geos, proj, tiff, and gdal dependencies RPMS" >> $LOG
+   $RM atlas atlas-devel
+   $RM blas blas-devel
+   $RM lapack lapack-devel
+   $RM libaec libaec-devel
+   $RM hdf5 hdf5-devel
+   
+   echo "Removing SHARED lifemapper-* and prerequisite RPMS" >> $LOG	
    $RM lifemapper-cctools
+   $RM lifemapper-gdal
+   $RM lifemapper-geos
+   $RM lifemapper-proj
+   
+   echo "Removing SHARED data RPMS" >> $LOG
    $RM lifemapper-env-data
+   
    echo "Removing SHARED opt-* RPMS" >> $LOG
+   $RM opt-lifemapper-cython
+   $RM opt-lifemapper-dendropy
    $RM opt-lifemapper-egenix-mx-base
    $RM opt-lifemapper-requests
-   $RM opt-lifemapper-dendropy
+}
+
+del-shared-directories() {
+   echo "Removing lifemapper installation directory" >> $LOG
+   rm -rf /opt/lifemapper
+   echo "Removing shared lifemapper temp and data directories" >> $LOG
+   rm -rf /state/partition1/lmscratch
+   rm -rf /state/partition1/lm
+   echo "Removing shared lifemapper PID directory" >> $LOG
+   rm -rf /var/run/lifemapper
+}
+
+del-shared-user-group () {
+   if [ $LMUSER_COUNT = 1 ] ; then
+       echo "Remove lmwriter user/group/dirs" >> $LOG
+       userdel lmwriter
+       groupdel lmwriter
+       /bin/rm -f /var/spool/mail/lmwriter
+       /bin/rm -rf /export/home/lmwriter
+       echo "Syncing users info" >> $LOG
+       rocks sync users
+   fi
 }
 
 del-lifemapper() {
@@ -83,43 +77,16 @@ del-lifemapper() {
 
 del-opt-python () {
    echo "Removing opt-* RPMS" >> $LOG
-   $RM opt-lifemapper-pyparsing
+#    $RM opt-lifemapper-pyparsing
    $RM opt-lifemapper-scipy
 }
 
-del-directories () {
-   if [ $LMROLL_COUNT = 1 ]; then
-      echo "Removing shared /opt/lifemapper" >> $LOG
-      rm -rf /opt/lifemapper
-      echo "Removing shared data directories" >> $LOG
-      rm -rf /state/partition1/lmscratch
-      rm -rf /state/partition1/lm
-      echo "Removing shared PID directory" >> $LOG
-      rm -rf /var/run/lifemapper
-   fi
-
+del-node-directories () {
    echo "Removing node code, data and PID directories" >> $LOG
    rocks run host compute "rm -rf /opt/lifemapper"
    rocks run host compute "rm -rf /state/partition1/lm"
    rocks run host compute "rm -rf /state/partition1/lmscratch"
    rocks run host compute "rm -rf /var/run/lifemapper"
-}
-
-del-user-group () {
-   needSync=0
-   if [ $LMUSER_COUNT = 1 ] && [ $LMROLL_COUNT = 1 ]; then
-       echo "Remove lmwriter user/group/dirs" >> $LOG
-       userdel lmwriter
-       groupdel lmwriter
-       /bin/rm -f /var/spool/mail/lmwriter
-       /bin/rm -rf /export/home/lmwriter
-       needSync=1
-   fi
-
-   if [ "$needSync" -eq "1" ]; then
-       echo "Syncing users info" >> $LOG
-       rocks sync users
-   fi
 }
 
 # remove obsolete Lifemapper cron jobs
@@ -164,16 +131,19 @@ check_lm_processes
 
 set_defaults
 TimeStamp "# Start"
+
 if [ $LMROLL_COUNT = 1 ]; then
-	del-shared-geospatial-dependencies
 	del-lifemapper-shared
+	del-shared-directories
+	del-shared-user-group
 fi
 
 del-opt-python 
 del-lifemapper
-del-directories
+del-node-directories
 del-user-group
 del-cron-jobs
 del-automount-entry
 del-roll
+
 TimeStamp "# End"
